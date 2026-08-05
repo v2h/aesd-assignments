e9032718ec96cc65e81a809e240d905b804942db
v2h <viethungvu@hotmail.com>
2026-08-05 10:03:01 +0100

    Make unit-test.sh run

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,15 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // 1) Hard-coded username from autotest-validate.c
+    const char *hardcoded_username = my_username();
+
+    // 2) Username read from conf/username.txt (caller must free the buffer)
+    char *conf_username = malloc_username_from_conf_file();
+
+    // 3) The two must match
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, conf_username,
+        "my_username() does not match the username in conf/username.txt");
+
+    free(conf_username);
 }
--- unit-test.sh
+++ unit-test.sh
@@ -6,7 +6,7 @@
 # cmake .. && make && run the assignment-autotest application
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 make clean
 make
 cd ..
